5c3fd08194f6a13f407db090f80149e181179cf2
Justin Juarez <146039020+Jujuarez9801@users.noreply.github.com>
2023-12-14 19:31:42 -0800

    Update TempFile.java

--- TempFile.java
+++ TempFile.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 // Decimal to Binary
 
-public class TempFile {
+public class TempFileTestGit {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
